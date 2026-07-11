3897b67aad0b8667f2beb96c9be082fdbfb866c0
Tharnish palanisamy <tharnishpalanisamy3@gmail.com>
2026-07-11 16:16:32 +0530

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = [ [p , s]   for p , s in zip(position , speed)] 
+        pairs.sort() 
+
+        stack = [] 
+
+        for p , s in pairs[::-1] : 
+            stack.append((target-p) / s) 
+
+            if len(stack) >= 2 and stack[-1] <= stack[-2] : 
+                stack.pop() 
+        return len(stack) 
