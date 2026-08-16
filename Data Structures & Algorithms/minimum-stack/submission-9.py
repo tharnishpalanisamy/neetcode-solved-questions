339ea0562d6cb8eb8ad56c4e70ebb2fdcb74c7b0
Tharnish palanisamy <tharnishpalanisamy3@gmail.com>
2026-08-16 15:10:53 +0530

    Add: minimum-stack - submission-9

---
+++ Data Structures & Algorithms/minimum-stack/submission-9.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = [] 
+        self.min = []
+        
+
+    def push(self, val: int) -> None:
+        if (self.min and val <= self.min[-1] ) or not self.min  :
+            self.min.append(val) 
+        self.stack.append(val) 
+        
+
+    def pop(self) -> None:
+        if self.stack :
+            if self.min and self.min[-1] == self.stack[-1] :
+                self.min.pop()
+            self.stack.pop()
+
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min[-1] if self.min else 0
+        
