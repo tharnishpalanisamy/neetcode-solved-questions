b958cdee96fed880667bbb38c5a11392b220076d
Tharnish palanisamy <tharnishpalanisamy3@gmail.com>
2026-09-23 18:25:19 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int: 
+        if n <= 2 : 
+            return n
+        prev1 = 2
+        prev2 = 1 
+        for i in range(3 , n+1) : 
+            res = prev1 + prev2 
+            prev2 = prev1 
+            prev1 = res 
+        return prev1 
